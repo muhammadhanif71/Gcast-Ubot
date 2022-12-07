77d1b91c03dde2de76090d85f9246af557e388d7
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-12-08 04:32:35 +0700

    Update gcast.py

--- AyiinXd/gcast.py
+++ AyiinXd/gcast.py
@@ -45,6 +45,7 @@ GCAST_BLACKLIST = [
     -1001662510083,  # MutualanDestra
     -1001347414136,  # ArunaMutualan
     -1001572486389,  # PluviaMusicGroup
+    -1001601365018,  ,# stufsupport
 ]
 
 
